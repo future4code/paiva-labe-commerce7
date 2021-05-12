b97156d2e4a25b02eeb77cc59133a52af49d884e
Thiago Felipe <thiago.felipe@tutanota.com>
2021-05-12 15:35:37 -0400

    Fazendo a parte de mostrar produtos e adicionar produtos no carrinho

---
+++ src/components/Produto.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+
+const ProdutoArticle = styled.article`
+  border: 1px solid #000000;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ImagemProduto = styled.img`
+  width: 100%;
+`;
+
+const Titulo = styled.h3`
+  text-align: center;
+`;
+
+const Valor = styled.p`
+  text-align: center;
+  &::before {
+    content: "R$  "
+  }
+`;
+
+const Adicionar = styled.button`
+  margin: 1em auto;
+  cursor: pointer;
+  transition: transform 200ms;
+  &:active {
+    transform: translateY(4px);
+  }
+`;
+
+function Produto( props ) {
+
+  const { imagem, nome, valor } = props.produto;
+
+  return (
+    < ProdutoArticle >
+      < ImagemProduto src = { imagem } />
+      < Titulo >{ nome }</ Titulo >
+      < Valor >{ valor }</ Valor >
+      < Adicionar onClick = { () => props.adicionarNoCarrinho( props.produto ) } >
+        Adicionar No Carrinho
+      </ Adicionar >
+    </ ProdutoArticle >
+  );
+
+}
+
+export default Produto;
