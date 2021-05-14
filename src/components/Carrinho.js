f480ae83dcefa84ca74fd0948472aa6a58332ae5
lausmpc <lauranunesc.@gmail.com>
2021-05-14 14:41:29 -0300

    Corrigida implementação no componente

--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -32,16 +32,27 @@ class Carrinho extends Component {
   }
 
   onClickDeletar = (idParaDeletar) => {
-    const novaLista = [ ...this.state.carrinho ];
-    const listaFiltrada = novaLista
-      .filter((produto) => produto.id !== idParaDeletar);
+    const novaLista = [...this.state.carrinho];
+    const listaFiltrada = novaLista.filter((produto) => {
+      console.log(produto);
+      if (produto.quantidade > 1 && produto.id === idParaDeletar) {
+        produto.quantidade -= 1;
+        return produto
+      } else {
+        return produto.id !== idParaDeletar;
+      }
+    });
+    console.log(listaFiltrada);
 
     let total = 0;
-    listaFiltrada.forEach((produto) => total += produto.valor * produto.quantidade);
+    listaFiltrada.forEach(
+      (produto) => (total += produto.valor * produto.quantidade)
+    );
 
     this.setState({ total });
     this.setState({ carrinho: listaFiltrada });
   };
+
 
   render() {
     return (
